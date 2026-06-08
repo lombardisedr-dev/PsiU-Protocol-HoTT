1fbccbc8cb40ab2b54866f4bbb129003491be57d
lombardisedr-dev <lombardisedr@gmail.com>
2026-06-08 13:14:13 +0200

    Create PsiU_Generate_Validation_Plot.R with placeholder
    Add temporary placeholder to avoid pipeline crash

---
+++ PsiU_Generate_Validation_Plot.R
@@ -0,0 +1,3 @@
+# PsiU_Generate_Validation_Plot.R
+# Segnaposto temporaneo per evitare il crash della pipeline
+cat("Generazione del plot di validazione omotopica saltata: file non implementato.\n")
