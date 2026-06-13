289c7f74be4dfecc9001433f573ea6c40df34cf9
lombardisedr-dev <lombardisedr@gmail.com>
2026-06-13 17:59:32 +0200

    Update engine_2.0.R

--- R/engine_2.0.R
+++ R/engine_2.0.R
@@ -1,11 +1,7 @@
-# ==============================================================================
-#      ENGINE PSI-U COMPLETO CON MULTILIBRARY E PRINT INQUADRATO (V3)
-# ==============================================================================
-
 PsiU_Complete_MultiLibrary_V3 <- function(raw_input_vector, block_size = 50000, critical_threshold = 71.07) {
-  # Tempo di inizio computazione
   tempo_inizio <- Sys.time()
   
+  # Costanti Logiche
   MATTER_THRESHOLD <- 1/3
   SPACE_THRESHOLD  <- 2/3
   G_COSMIC         <- (sqrt(5) - 1) / 3  
@@ -13,8 +9,8 @@ PsiU_Complete_MultiLibrary_V3 <- function(raw_input_vector, block_size = 50000, 
   N_total  <- length(raw_input_vector)
   n_blocks <- floor(N_total / block_size)
   
+  # Strutture di Memoria
   MULTILIBRARY <- list("M1" = numeric(0), "M2" = numeric(0), "M3" = numeric(0))
-  
   volumi_box <- 0; volumi_diamond <- 0; volumi_transizione <- 0; volumi_vuoto <- 0
   rami_recisi <- 0
   
@@ -32,50 +28,47 @@ PsiU_Complete_MultiLibrary_V3 <- function(raw_input_vector, block_size = 50000, 
     idx_end   <- b * block_size
     block_grezzo <- raw_input_vector[idx_start:idx_end]
     
+    # Proiezione Modale
     val_p <- (block_grezzo - min_globale) / rng_globale
-    
-    is_box         <- val_p <= MATTER_THRESHOLD
-    is_vuoto       <- val_p > SPACE_THRESHOLD
-    in_space       <- !is_box & !is_vuoto
+    is_box <- val_p <= MATTER_THRESHOLD
+    is_vuoto <- val_p > SPACE_THRESHOLD
+    in_space <- !is_box & !is_vuoto
     
     diamond_tolerance <- (SPACE_THRESHOLD - MATTER_THRESHOLD) * 0.5 
-    dist_g            <- abs(val_p - G_COSMIC)
-    is_diamond        <- in_space & (dist_g <= diamond_tolerance)
-    is_transizione    <- in_space & !is_diamond
+    is_diamond <- in_space & (abs(val_p - G_COSMIC) <= diamond_tolerance)
+    is_transizione <- in_space & !is_diamond
     
-    volumi_box         <- volumi_box + sum(is_box)
-    volumi_diamond     <- volumi_diamond + sum(is_diamond)
+    # Accumulo Volumi
+    volumi_box <- volumi_box + sum(is_box)
+    volumi_diamond <- volumi_diamond + sum(is_diamond)
     volumi_transizione <- volumi_transizione + sum(is_transizione)
-    volumi_vuoto       <- volumi_vuoto + sum(is_vuoto)
+    volumi_vuoto <- volumi_vuoto + sum(is_vuoto)
     
     diamond_grezzo <- block_grezzo[is_diamond]
     
     if (b <= 3) {
+      # Fase di Imprinting DNA
       lib_name <- paste0("M", b)
       MULTILIBRARY[[lib_name]] <- block_grezzo[is_box | is_diamond]
       valori_risonanza_storici[b] <- mean(block_grezzo)
       indici_vicinanza_storici[b] <- critical_threshold
-      scostamenti_medi_storici[b] <- 0
     } else {
-      target_M1 <- mean(MULTILIBRARY$M1)
-      target_M2 <- mean(MULTILIBRARY$M2)
-      target_M3 <- mean(MULTILIBRARY$M3)
-      firma_risonanza_fissa <- mean(c(target_M1, target_M2, target_M3))
+      # Fase Operativa: Analisi di Risonanza
+      stabilità_base <- sd(c(MULTILIBRARY$M1, MULTILIBRARY$M2, MULTILIBRARY$M3))
+      targets <- sapply(MULTILIBRARY, mean)
+      firma_risonanza_fissa <- mean(targets)
       
       if (length(diamond_grezzo) > 0) {
-        scostamento_M1 <- mean(abs(diamond_grezzo - target_M1))
-        scostamento_M2 <- mean(abs(diamond_grezzo - target_M2))
-        scostamento_M3 <- mean(abs(diamond_grezzo - target_M3))
-                scostamento_reale_minimo <- min(scostamento_M1, scostamento_M2, scostamento_M3)
+        # Calcolo scostamento dal baricentro gnomonico
+        scostamento_reale_minimo <- min(abs(mean(diamond_grezzo) - targets))
         
-        # Calcolo scientifico: quanto devia il blocco rispetto alla stabilità del DNA base
-        stabilità_base <- sd(c(MULTILIBRARY$M1, MULTILIBRARY$M2, MULTILIBRARY$M3))
-        vicinanza_blocco <- (1 - (scostamento_reale_minimo / (stabilità_base * 4))) * 100
-
-
+        # FILTRO DI SENSIBILITA' (1.5x SD per rigore scientifico)
+        vicinanza_blocco <- (1 - (scostamento_reale_minimo / (stabilità_base * 1.5))) * 100
+        
         if (vicinanza_blocco < critical_threshold) {
-          rami_recisi <- rami_recisi + 1
+            rami_recisi <- rami_recisi + 1
         }
+        
         indici_vicinanza_storici[b] <- vicinanza_blocco
         scostamenti_medi_storici[b] <- scostamento_reale_minimo
       } else {
@@ -83,75 +76,49 @@ PsiU_Complete_MultiLibrary_V3 <- function(raw_input_vector, block_size = 50000, 
         scostamenti_medi_storici[b] <- rng_globale
         rami_recisi <- rami_recisi + 1
       }
-      # Calcolo scientifico: tolleranza ridotta per massima sensibilità
-vicinanza_blocco <- (1 - (scostamento_reale_minimo / (stabilità_base * 1.5))) * 100
-
+      valori_risonanza_storici[b] <- firma_risonanza_fissa
     }
   }
   
-  tempo_fine <- Sys.time()
-  tempo_calcolo <- as.numeric(difftime(tempo_fine, tempo_inizio, units = "secs"))
-  
-  # Compilazione dell'oggetto strutturato con classe specifica per il print
+  # Compilazione Report
+  tempo_calcolo <- as.numeric(difftime(Sys.time(), tempo_inizio, units = "secs"))
   report <- list(
     METRICHE_SCALA = c(
-      "Volume Totale Flusso (Input)"   = N_total,
-      "Dimensione del Vettore Dati"    = object.size(raw_input_vector) / (1024^2), # in MB
-      "Rami Recisi per Esclusione"     = rami_recisi
+      "Volume Totale Flusso"   = N_total,
+      "Dimensione Vettore MB"  = as.numeric(object.size(raw_input_vector) / (1024^2)),
+      "Rami Recisi (Anomalie)" = rami_recisi
     ),
     VOLUMI_MODALI = data.frame(
-      Volume_Assoluto   = c(volumi_box, volumi_diamond, volumi_transizione, volumi_vuoto),
-      Quota_Percentuale = c((volumi_box/N_total)*100, (volumi_diamond/N_total)*100, (volumi_transizione/N_total)*100, (volumi_vuoto/N_total)*100),
-      row.names         = c("BOX (Materia / Necessita') [ ]", "DIAMOND (Ereditarieta' / Possibilita') [◆]", "TRANSIZIONE OMOTOPICA (Flusso Spaziale)", "VUOTO ESPANSIVO (Mutazione Aperta)")
+      Volume_Assoluto = c(volumi_box, volumi_diamond, volumi_transizione, volumi_vuoto),
+      Quota_Percentuale = c(volumi_box, volumi_diamond, volumi_transizione, volumi_vuoto)/N_total*100,
+      row.names = c("BOX (Materia) [ ]", "DIAMOND (Ereditarieta') [◆]", "TRANSIZIONE OMOTOPICA", "VUOTO ESPANSIVO")
     ),
     DIAGNOSTICA = c(
-      "Firma di Risonanza (Target Fisso)" = tail(valori_risonanza_storici, 1),
-      "Scostamento Medio dal DNA Base"    = mean(scostamenti_medi_storici[4:n_blocks], na.rm=TRUE),
-      "Indice di Vicinanza Generale"      = mean(indici_vicinanza_storici[4:n_blocks], na.rm=TRUE)
+      "Firma Risonanza Target" = tail(valori_risonanza_storici, 1),
+      "Scostamento Medio"      = mean(scostamenti_medi_storici[4:n_blocks], na.rm=TRUE),
+      "Vicinanza Generale %"   = mean(indici_vicinanza_storici[4:n_blocks], na.rm=TRUE)
     ),
     BENCHMARK_PRESTAZIONI = c(
-      "Tempo di Calcolo CPU Effettivo"   = tempo_calcolo,
-      "Velocita' Elaborazione (Through)" = N_total / tempo_calcolo
+      "Tempo Calcolo CPU"      = tempo_calcolo,
+      "Throughput (rec/sec)"   = N_total / tempo_calcolo
     )
   )
-  
   class(report) <- "PsiU_Inquadrato_Report"
   return(report)
 }
 
-# Metodo S3 di Stampa Inquadrato e Allineato
+# Metodo di Stampa
 print.PsiU_Inquadrato_Report <- function(x, ...) {
-  sep_line <- "========================================================================"
-  sub_line <- "------------------------------------------------------------------------"
-  
-  cat("\n", sep_line, "\n", sep = "")
-  cat("           BENCHMARK METRICO INDUSTRIALE - ARCHITETTURA PSI-U           \n")
-  cat(sep_line, "\n\n")
-  
-  cat(" STRUTTURA DELL'ALBERO COMPUTAZIONALE E SCALA\n")
-  cat(sub_line, "\n")
-  cat(sprintf("  %-40s : %d\n", "Volume Totale Analizzato", x$METRICHE_SCALA[1]))
-  cat(sprintf("  %-40s : %.2f MB\n", "Dimensione Vettore in Memoria", x$METRICHE_SCALA[2]))
-  cat(sprintf("  %-40s : %d\n", "Rami Recisi per Esclusione (4+)", x$METRICHE_SCALA[3]))
-  
-  cat("\n MAPPATURA INTEGRALE DEI VOLUMI MODALI\n")
-  cat(sub_line, "\n")
-  vol_df <- x$VOLUMI_MODALI
-  for(i in 1:nrow(vol_df)) {
-    cat(sprintf("  %-42s | Vol: %-8d | Quota: %6.3f%%\n", 
-                rownames(vol_df)[i], vol_df$Volume_Assoluto[i], vol_df$Quota_Percentuale[i]))
-  }
-  
-  cat("\n DIAGNOSTICA DI RISONANZA E COERENZA OMOTOPIA\n")
-  cat(sub_line, "\n")
-  cat(sprintf("  %-40s : %.5f\n", "Firma Reale di Risonanza (Target)", x$DIAGNOSTICA[1]))
-  cat(sprintf("  %-40s : %.5f\n", "Scostamento Medio dal DNA di Base", x$DIAGNOSTICA[2]))
-  cat(sprintf("  %-40s : %.2f%%\n", "Indice di Vicinanza Generale", x$DIAGNOSTICA[3]))
-  
-  cat("\n METRICHE DI VELOCITA' INDUSTRIALE COMPILATE\n")
-  cat(sub_line, "\n")
-  cat(sprintf("  %-40s : %.4f secondi\n", "Tempo di Calcolo CPU", x$BENCHMARK_PRESTAZIONI[1]))
-  cat(sprintf("  %-40s : %.2f record/secondo\n", "Throughput Operativo", x$BENCHMARK_PRESTAZIONI[2]))
-  
-  cat(sep_line, "\n")
+  sep <- "========================================================================"
+  cat("\n", sep, "\n           BENCHMARK METRICO INDUSTRIALE - ARCHITETTURA PSI-U           \n", sep, "\n\n")
+  cat(" STRUTTURA DELL'ALBERO E SCALA\n------------------------------------------------------------------------\n")
+  for(i in 1:3) cat(sprintf("  %-40s : %s\n", names(x$METRICHE_SCALA)[i], format(x$METRICHE_SCALA[i], scientific=F)))
+  cat("\n MAPPATURA VOLUMI MODALI\n------------------------------------------------------------------------\n")
+  df <- x$VOLUMI_MODALI
+  for(i in 1:4) cat(sprintf("  %-42s | Vol: %-8d | Quota: %6.3f%%\n", rownames(df)[i], df[i,1], df[i,2]))
+  cat("\n DIAGNOSTICA DI RISONANZA\n------------------------------------------------------------------------\n")
+  for(i in 1:3) cat(sprintf("  %-40s : %.5f\n", names(x$DIAGNOSTICA)[i], x$DIAGNOSTICA[i]))
+  cat("\n PERFORMANCE\n------------------------------------------------------------------------\n")
+  cat(sprintf("  %-40s : %.4f sec\n  %-40s : %.2f\n", names(x$BENCHMARK_PRESTAZIONI)[1], x$BENCHMARK_PRESTAZIONI[1], names(x$BENCHMARK_PRESTAZIONI)[2], x$BENCHMARK_PRESTAZIONI[2]))
+  cat(sep, "\n")
 }
